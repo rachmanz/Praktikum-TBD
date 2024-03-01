2b8558f676d62275a37b291b1a0ee163ef834142
KingMeowz <kita.dev1905@gmail.com>
2024-03-01 11:29:42 +0700

    Done Latihan

---
+++ Modul-1 (XML dan SQLite)/121450128/sqlite/tugas-sqlite.py
@@ -0,0 +1,69 @@
+import sqlite3 as sq3
+
+def create_table():
+    conn = sq3.connect("D:/My Wish/Collage Data/Semester 6/Teknologi Basis Data/Praktikum-TBD/Modul-1 (XML dan SQLite)/121450128/sqlite/mahasiswa.sqlite")
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS mahasiswa
+                (id INTEGER PRIMARY KEY, name TEXT, prodi TEXT)''')
+
+    conn.commit() 
+    conn.close() 
+
+    print('Table berhasil dibuat')
+
+create_table() 
+
+def addmahasiswa(id, name, prodi):
+    conn = sq3.connect('D:/My Wish/Collage Data/Semester 6/Teknologi Basis Data/Praktikum-TBD/Modul-1 (XML dan SQLite)/121450128/sqlite/mahasiswa.sqlite')
+    c = conn.cursor() 
+    c.execute("INSERT INTO mahasiswa (id, name, prodi) VALUES (?, ?, ?)", (id, name, prodi))
+    conn.commit() 
+    conn.close()
+
+    print("User berhasil ditambahkan")
+
+def delete_mahasiswa(id):
+    conn = sq3.connect("D:/My Wish/Collage Data/Semester 6/Teknologi Basis Data/Praktikum-TBD/Modul-1 (XML dan SQLite)/121450128/sqlite/mahasiswa.sqlite")
+    c = conn.cursor() 
+    c.execute("DELETE FROM mahasiswa WHERE id = ?", (id,)) 
+    conn.commit() 
+    conn.close() 
+
+    print("Mahasiswa berhasil di drop out (DO)")
+
+def get_all_mahasiswa():
+    conn = sq3.connect("D:/My Wish/Collage Data/Semester 6/Teknologi Basis Data/Praktikum-TBD/Modul-1 (XML dan SQLite)/121450128/sqlite/mahasiswa.sqlite")
+    c = conn.cursor() 
+    c.execute("SELECT * FROM mahasiswa")
+    rows = c.fetchall()
+    conn.close()
+    return rows
+
+def update_mahasiswa(id, name, prodi): 
+    conn = sq3.connect("D:/My Wish/Collage Data/Semester 6/Teknologi Basis Data/Praktikum-TBD/Modul-1 (XML dan SQLite)/121450128/sqlite/mahasiswa.sqlite")
+    c = conn.cursor()
+    c.execute("UPDATE mahasiswa SET name = ?, prodi = ? WHERE id = ?", (name, prodi, id))
+    conn.commit() 
+    conn.close() 
+
+    print("Mahasiswa berhasil ditambahkan")
+
+# Menambahkan Mahasiswa 
+addmahasiswa(101, "Arya Wicaksono Pratama", "Program Studi Teknik Industri")
+addmahasiswa(102, "Sofia Elara", "Program Studi Teknik Pertambangan")
+addmahasiswa(103, "Abdurrahman Al-atsary", "Program Studi Sains Data")
+
+# Mencetak seluruh mahasiswa 
+print("All Mahasiswa")
+print(get_all_mahasiswa())
+
+# Update Mahasiswa 
+update_mahasiswa(101, "Arya Wicaksono Pratama", "Program Studi Teknik Perkeretaapian")
+update_mahasiswa(103, "Abdurrahman Al-atsary", "Program Studi Arsitektur Lanskap")
+print("After Update:")
+print(get_all_mahasiswa())
+
+# Delete Mahasiswa 
+delete_mahasiswa(102)
+print("After delete:")
+print(get_all_mahasiswa())
--- Modul-1 (XML dan SQLite)/121450128/xml/show-by-jurusan.py
+++ Modul-1 (XML dan SQLite)/121450128/xml/show-by-jurusan.py
@@ -1,17 +1,12 @@
 import xml.etree.ElementTree as ET
 
-# Membaca file xml 
-tree = ET.parse("D:/SMT 6/Praktikum TBD/Praktikum-TBD/Modul-1 (XML dan SQLite)/121450128/xml/mahasiswa.xml")
+# Membaca file xml
+tree = ET.parse("D:/My Wish/Collage Data/Semester 6/Teknologi Basis Data/Praktikum-TBD/Modul-1 (XML dan SQLite)/121450128/xml/mahasiswa.xml")
 root = tree.getroot()
 
-for mahasiswa in root.findall("./mahasiswa[jurusan='Arsitektur']"):
-    nim = mahasiswa.find("nim").text
-    nama = mahasiswa.find("nama").text
-    alamat = mahasiswa.find("alamat").text
+# Loop through mahasiswa elements with jurusan 'Arsitektur'
+for data in root.findall("./data[jurusan='Arsitektur']"):
+    nim = data.get("nim")
+    nama = data.find("nama").text
+    alamat = data.find("alamat").text
     print(f"NIM: {nim}, Nama: {nama}, Alamat: {alamat}")
-# Lengkapi Code sesuai output ini
-
-# Output; 
-# Mahasiswa dengan jurusan Arsitektur 
-# NIM: 111222333, Nama: Charlie, Alamat: Lampung
-# NIM: 444555666, Nama: Diana, Alamat: Yogyakarta
